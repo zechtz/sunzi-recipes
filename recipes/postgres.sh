2ce6135ca4ac88aca99fb17b97f8a68d8056b8e1
Zachariah Ngonyani <zachariahn@gmail.com>
2017-10-28 23:39:58 +0300

    properly set up postgres ppa source list

--- recipes/postgres.sh
+++ recipes/postgres.sh
@@ -3,16 +3,16 @@
 if aptitude search '~i ^postgresql$' | grep -q postgresql; then
   echo "postgresql already installed, skipping."
 else
-  echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" >> /etc/apt/sources.list.d/postgresql-pgdgp-trusty.list
+  echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
   wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-  apt-get update
-  apt-get -y install postgresql libpq-dev
+  apt-get update -y
+  apt-get -y install postgresql-<%= @attributes.postgres_version %> libpq-dev
 
-   #enable password authentication
-   mv files/pg_hba.conf /etc/postgresql/<%= @attributes.postgres_version %>/main/pg_hba.conf
-   chown postgres:postgres /etc/postgresql/<%= @attributes.postgres_version %>/main/pg_hba.conf
-   chmod 640 /etc/postgresql/<%= @attributes.postgres_version %>/main/pg_hba.conf
-   sudo service postgresql restart
+  #enable password authentication
+  mv files/pg_hba.conf /etc/postgresql/<%= @attributes.postgres_version %>/main/pg_hba.conf
+  chown postgres:postgres /etc/postgresql/<%= @attributes.postgres_version %>/main/pg_hba.conf
+  chmod 640 /etc/postgresql/<%= @attributes.postgres_version %>/main/pg_hba.conf
+  sudo service postgresql restart
 
   #create user and database
   sudo -u postgres psql -c "create user <%= @attributes.postgres_user %> with password '<%= @attributes.postgres_password %>';"
