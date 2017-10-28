4c1e13f5708f605837ba7cc3c21b783816705c61
Zachariah Ngonyani <zachariahn@gmail.com>
2017-10-28 23:38:02 +0300

    remove ownership assignment task from nginx recipe

--- recipes/nginx.sh
+++ recipes/nginx.sh
@@ -1,16 +1,16 @@
 if aptitude search '~i ^nginx$' | grep -q nginx; then
   echo "nginx already installed, skipping."
 else
+
   add-apt-repository ppa:nginx/stable --yes
   apt-get -y update
   apt-get -y install nginx
 
-  mkdir -p /var/www/html/ffars/public
-  chown -R root:root /var/www/html/ffars/public/
+  mkdir -p /var/www/html/<%=@attributes.deploy_user%>/public
   chmod 755 /var/www
-  mv files/nginx.conf /etc/nginx/sites-available/ffars
+  mv files/nginx.conf /etc/nginx/sites-available/<%= @attributes.app_name %>
 
-  sudo ln -s /etc/nginx/sites-available/ffars /etc/nginx/sites-enabled/ffars
-  echo 'Webserver provisioned by sunzi is working' > /var/www/html/ffars/public/index.html
+  sudo ln -s /etc/nginx/sites-available/<%= @attributes.app_name%> /etc/nginx/sites-enabled/<%= @attributes.app_name%>
+  echo 'Webserver provisioned by sunzi is working' > /var/www/html/<%= @attributes.app_name%>/public/index.html
   service nginx restart
 fi
