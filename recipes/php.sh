d597d6258a4e3a11ac1fb8da7bd12ce87763fccb
Zachariah Ngonyani <zachariahn@gmail.com>
2017-10-28 23:39:14 +0300

    clean up php recipe and add modules

--- recipes/php.sh
+++ recipes/php.sh
@@ -1,53 +1,8 @@
-# php provision
-
-if [ "$sunzi_pkg" = 'apt-get' ]; then
-  cnfdir='/etc/nginx/conf.d/'
-  sitedir='/etc/nginx/sites-available/'
+if aptitude search '~i ^php$' | grep -q nodejs; then
+  echo "php already installed, skipping."
 else
-  exit;
-fi
-
-if ! sunzi.installed "nginx"; then
-  sunzi.install "nginx"
-fi
-
-if ! sunzi.installed "php5.6-common"; then
-  sunzi.mute "apt-get install -qqy php5.6 php5.6-common php5.6-apc php5.6-gd php5.6-fpm php5.6-cli"
-  echo "PHP5-FPM configuration setup.."
-  cnf="php.cnf"
-  targetfile=${cnfdir}${cnf}
-  rm -f $targetfile
-  cat >> $targetfile <<EOM
-  index index.php index.html index.htm;
-
-  # upstream to abstract backend connection for PHP.
-  upstream php {
-  server unix:/var/run/php5-fpm.sock;
-}
-EOM
-site='phpsample'
-targetfile=${sitedir}${site}
-rm -f $targetfile
-cat >> $targetfile <<'EOM'
-# sample php sites
-server {
-server_name _;
-root /var/www;
-
-location / {
-try_files $uri $uri/index.php?$args;
-    }
-
-    location ~ \.php$ {
-    try_files $uri =404;
-    fastcgi_split_path_info ^(.+\.php)(/.+)$;
-    #NOTE: You should have "cgi.fix_pathinfo = 0;" in php.ini
-
-    include fastcgi_params;
-    fastcgi_index index.php;
-    fastcgi_intercept_errors on;
-    fastcgi_pass php;
-  }
-}
-EOM
+  apt-get install python-software-properties --yes
+  add-apt-repository ppa:ondrej/php
+  apt-get update -y
+  apt-get install -y php5.6 php5.6-mcrypt php5.6-bcmath php5.6-gd php5.6-pdo php5.6-pdo_pgsql php5.6-pgsql
 fi
