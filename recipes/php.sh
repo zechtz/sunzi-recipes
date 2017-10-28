09370844fd843ba0f6d06734c02d70d29ed93a1d
Zachariah Ngonyani <zachariahn@gmail.com>
2017-10-29 01:09:07 +0300

    install require php module and add apt-get and apt-add-repository to sudo no password

--- recipes/php.sh
+++ recipes/php.sh
@@ -4,5 +4,5 @@ else
   apt-get install python-software-properties --yes
   add-apt-repository ppa:ondrej/php
   apt-get update -y
-  apt-get install -y php5.6 php5.6-mcrypt php5.6-bcmath php5.6-gd php5.6-pdo php5.6-pdo_pgsql php5.6-pgsql
+  apt-get install php5.6 php5.6-mcrypt php5.6-bcmath php5.6-gd php5.6-pdo php5.6-pdo-pgsql php5.6-pgsql -y
 fi
