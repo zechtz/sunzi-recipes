d50dc0598777eb5f775926687da50a82d3ee1c52
Zachariah Ngonyani <zachariahn@gmail.com>
2017-10-29 01:43:25 +0300

    add recipes to install composer and create folders

--- recipes/adduser.sh
+++ recipes/adduser.sh
@@ -9,5 +9,6 @@ else
   mv files/deploy_key /home/<%= @attributes.deploy_user %>/.ssh/authorized_keys
   chown -R <%= @attributes.deploy_user %>:<%= @attributes.deploy_user %> /home/<%= @attributes.deploy_user %>/.ssh
   chmod 400 /home/<%= @attributes.deploy_user %>/.ssh/authorized_keys
+  chown <%= @attributes.deploy_user %>:<%= @attributes.deploy_user %> /var/www/
   chown -R <%= @attributes.deploy_user %>:www-data /var/www/html/
 fi
---
+++ recipes/folders.sh
@@ -0,0 +1,4 @@
+mkdir /var/www/html/<%=attributes.app_name%>/storage
+mkdir /var/www/html/<%=attributes.app_name%>/shared
+chmod -R 775 /var/www/html/<%=attributes.app_name%>/storage
+chmod -R 775 /var/www/html/<%=attributes.app_name%>/shared
