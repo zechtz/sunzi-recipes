4b4cd429ae261853136f023c1785f3e247520c29
Zachariah Ngonyani <zachariahn@gmail.com>
2017-10-29 10:29:22 +0300

    do not generate ssh-key

--- recipes/adduser.sh
+++ recipes/adduser.sh
@@ -5,7 +5,6 @@ if [ -d /home/<%= @attributes.deploy_user %> ]; then
 else
   useradd <%= @attributes.deploy_user %> -m -s /bin/bash
   mkdir -p /home/<%= @attributes.deploy_user %>/.ssh
-  ssh-keygen -b 2048 -t rsa -f /home/<%= @attributes.deploy_user %>/.ssh/id_rsa.pub -q -N ""
   mv files/deploy_key /home/<%= @attributes.deploy_user %>/.ssh/authorized_keys
   chown -R <%= @attributes.deploy_user %>:<%= @attributes.deploy_user %> /home/<%= @attributes.deploy_user %>/.ssh
   chown <%= @attributes.deploy_user %>:<%= @attributes.deploy_user %> /etc/nginx/sites-available/<%=@attributes.app_name%>
