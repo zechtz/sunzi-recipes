42b9829afb28a4f556a861f5265a21c093c8c4cb
Zachariah Ngonyani <zachariahn@gmail.com>
2017-10-29 03:30:05 +0300

    update php script

--- recipes/php.sh
+++ recipes/php.sh
@@ -4,5 +4,5 @@ else
   apt-get install python-software-properties --yes
   add-apt-repository ppa:ondrej/php
   apt-get update -y
-  apt-get install php5.6 php5.6-mcrypt php5.6-bcmath php5.6-gd php5.6-pdo php5.6-pdo-pgsql php5.6-pgsql -y
+  apt-get install php5.6 php5.6-mcrypt php5.6-bcmath php5.6-gd php5.6-pdo php5.6-pdo-pgsql php5.6-pgsql php5.6-mbstring php5.6-tokenizer -y
 fi
