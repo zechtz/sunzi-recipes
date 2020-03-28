0bda0a82e763535b6095798689df3266840d37d5
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-28 20:54:22 +0300

    Fix error in pg_hba conf"

--- recipes/postgres.sh
+++ recipes/postgres.sh
@@ -8,23 +8,17 @@ else
   wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
   echo "deb http://apt.postgresql.org/pub/repos/apt/ $DISTRO-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql.list
   apt-get update -y
-  apt-get -y install postgresql-<%= @vars.postgres_version %> libpq-dev
-
-  if [ -f "/etc/postgresql/10/main/pg_hba.conf" ]; then
-    echo "pg_hba.conf already exists"
-    rm /etc/postgresql/10/main/pg_hba.conf
-  fi
+  apt-get -y install postgresql-<%=@vars.postgres_version%> postgresql-contrib
 
   #enable password authentication
-  /etc/init.d/postgresql restart
-
-  mv files/pg_hba.sample.conf /etc/postgresql/<%= @vars.postgres_version %>/main/pg_hba.conf
-  chown postgres:postgres /etc/postgresql/<%= @vars.postgres_version %>/main/pg_hba.conf
-  chmod 640 /etc/postgresql/<%= @vars.postgres_version %>/main/pg_hba.conf
-
-  /etc/init.d/postgresql restart
 
   #create user and database
   sudo -u postgres psql -c "create user <%= @vars.postgres_user %> with password '<%= @vars.postgres_password %>';"
   sudo -u postgres psql -c "create database <%= @vars.postgres_database %> owner <%= @vars.postgres_user %>;"
+
+  /etc/init.d/postgresql restart
+
+  mv files/pg_hba.conf /etc/postgresql/<%= @vars.postgres_version %>/main/
+  chown postgres:postgres /etc/postgresql/<%= @vars.postgres_version %>/main/pg_hba.conf
+  chmod 640 /etc/postgresql/<%= @vars.postgres_version %>/main/pg_hba.conf
 fi
