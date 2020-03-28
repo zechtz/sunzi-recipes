f2eab0873deeea136e1b06fb8718a461a8ae51ec
Zachariah Ngonyani <zachariahn@gmail.com>
2020-03-28 19:50:03 +0300

    Delete un-necessary recipes

--- recipes/install_php.sh
+++
@@ -1,9 +0,0 @@
-# Install php and required modules
-if apt search '~i ^php7.2$' | grep -q php; then
-  echo "php already installed, skipping."
-else
-  apt-get install software-properties-common --yes
-  add-apt-repository ppa:ondrej/php
-  apt-get update -y
-  apt-get install php7.2 php7.2-bcmath php7.2-gd  php7.2-fpm php7.2-pdo php7.2-pdo-pgsql php7.2-pgsql php7.2-mbstring php7.2-tokenizer php7.2-curl php7.2-json php7.2-xml php7.2-zip -y
-fi
--- recipes/postgres.sh
+++ recipes/postgres.sh
@@ -2,17 +2,21 @@
 
 DISTRO="$(lsb_release -s -c)"
 
-if aptitude search '~i ^postgresql-<%=@vars.postgres_version%>$' | grep -q postgresql; then
+if apt search '~i ^postgresql-<%=@vars.postgres_version%>$' | grep -q postgresql; then
   echo "postgresql already installed, skipping."
 else
-  echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
   wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
   echo "deb http://apt.postgresql.org/pub/repos/apt/ $DISTRO-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql.list
   apt-get update -y
   apt-get -y install postgresql-<%= @vars.postgres_version %> libpq-dev
 
+  if [ -f "/etc/postgresql/10/main/pg_hba.conf" ]; then
+    echo "pg_hba.conf already exists"
+    rm /etc/postgresql/10/main/pg_hba.conf
+  fi
+
   #enable password authentication
-  mv files/pg_hba.conf /etc/postgresql/<%= @vars.postgres_version %>/main/pg_hba.conf
+  mv files/pg_hba.sample.conf /etc/postgresql/<%= @vars.postgres_version %>/main/pg_hba.conf
   chown postgres:postgres /etc/postgresql/<%= @vars.postgres_version %>/main/pg_hba.conf
   chmod 640 /etc/postgresql/<%= @vars.postgres_version %>/main/pg_hba.conf
   sudo service postgresql restart
